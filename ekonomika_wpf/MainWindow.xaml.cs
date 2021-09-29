053aa6531a31bacd4fbb25c4e3bce66b851761c6
Alexey Mikhailyuk <35634279+alexmihalyk23@users.noreply.github.com>
2021-09-29 15:36:35 +0700

    Add files via upload

---
+++ ekonomika_wpf/MainWindow.xaml.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace ekonomika_wpf
+{
+    /// <summary>
+    /// Логика взаимодействия для MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void Credit_sum_PreviewTextInput(object sender, TextCompositionEventArgs e)
+        {
+            Regex regex = new Regex("[^0-9]+");
+            e.Handled = regex.IsMatch(e.Text);
+        }
+
+        private void calculate_Click(object sender, RoutedEventArgs e)
+        {
+            int credit_sum = int.Parse(Credit_sum.Text);
+            int procent_int = int.Parse(procent.Text);
+            double result = credit_sum * (procent_int * 0.01);
+            Result.Text = result.ToString();
+        }
+    }
+}
